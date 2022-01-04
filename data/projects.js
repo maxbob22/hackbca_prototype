b7dcb0955668db88f2bd889d7db8c3215c9730a5
Zach1031 <zachiscool1031@gmail.com>
2022-01-04 15:01:02 -0500

    Project details page created

--- data/projects.js
+++ data/projects.js
@@ -3,6 +3,7 @@ module.exports = [
         "project_name": "test",
         "project_creators": "Zach, Max, and Jeremy",
         "project_type": "website",
-        "project_email": "zacdef22@bergen.org"
+        "project_email": "zacdef22@bergen.org",
+        "project_id": "1234"
     }
 ]
