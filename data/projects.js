cbfd4552a3a60145a4e5f043a6c871f903d0c903
Zach1031 <zachiscool1031@gmail.com>
2022-01-04 15:07:22 -0500

    Fixed project details link

--- data/projects.js
+++ data/projects.js
@@ -4,6 +4,7 @@ module.exports = [
         "project_creators": "Zach, Max, and Jeremy",
         "project_type": "website",
         "project_email": "zacdef22@bergen.org",
-        "project_id": "1234"
+        "project_id": "1234",
+        "project_image": "/images/website.png"
     }
 ]
